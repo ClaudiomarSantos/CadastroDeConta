7a0f1d68a807260f738b59912c7bac77868ba376
Claudiomar <claudiomar.junior@aluno.unifametro.edu.br>
2020-02-12 13:20:44 -0300

    depósito inicial funcionando

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -1,10 +1,43 @@
 package aplicação;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.ContaBancaria;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Abrir conta ");
+		System.out.println("Digite o numero da conta: ");
+		int numConta = sc.nextInt();
+		sc.nextLine();
+		System.out.println("digite o nome completo do titular: ");
+		String name = sc.nextLine();
+		ContaBancaria cont = new ContaBancaria(numConta, name);
+
+		System.out.println("deseja fazer depósito inicial? s, ou n?");
+		char resp = sc.nextLine().charAt(0);
+		char resp1 = 's';
+		char resp2 = 'n';
+
+		while (resp != resp1 && resp != resp2) {
+			System.out.println("Resposta inválida");
+			System.out.println("deseja fazer depósito inicial? s, ou n?");
+			resp = sc.nextLine().charAt(0);
+
+		}
+		if (resp == resp1) {
+			System.out.println("digite o valor do depósito: ");
+			double saldo = sc.nextDouble();
+			cont.deposito(saldo);
+		} else if (resp == resp2) {};
+
+		System.out.println(cont.toString());
+
+		sc.close();
 	}
-
 }
--- src/entidades/ContaBancaria.java
+++ src/entidades/ContaBancaria.java
@@ -2,4 +2,44 @@ package entidades;
 
 public class ContaBancaria {
 
+	private int numConta;
+	private String name;
+	private double saldo = 0;
+
+	public ContaBancaria(int numConta, String name) {
+		this.numConta = numConta;
+		this.name = name;
+
+	}
+
+	public void deposito(double saldo) {
+		this.saldo += saldo;
+	}
+
+	public void saque(double saldo) {
+		this.saldo -= (saldo + 5);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+
+	public String toString() {
+		return "Numero da conta: " + numConta 
+				+ ", Nome Titular: " + getName() 
+				+ ", saldo: R$ " +  String.format("%.2f%n", getSaldo());
+	}
+
 }
